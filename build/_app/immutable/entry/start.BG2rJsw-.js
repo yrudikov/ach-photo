462a6fb01a7b4d26c918c555a36bce3b4dd9998b
rudi <yrudikov@gmail.com>
2025-07-20 21:40:21 +0200

    Change tel number

--- build/_app/immutable/entry/start.BG2rJsw-.js
+++
@@ -1,2 +0,0 @@
-import{l as s,a}from"../chunks/DMYaKB1Y.js";export{s as load_css,a as start};
-//# sourceMappingURL=start.BG2rJsw-.js.map
---
+++ build/_app/immutable/entry/start.D-Mz0Bii.js
@@ -0,0 +1,2 @@
+import{l as s,a}from"../chunks/CK17nkqA.js";export{s as load_css,a as start};
+//# sourceMappingURL=start.D-Mz0Bii.js.map
